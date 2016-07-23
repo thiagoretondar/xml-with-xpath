033d643fac2058252b08c1f7df7a9c832b286192
Thiago Retondar <t.retondar@gmail.com>
2016-07-22 23:20:57 -0300

    Initial commit

---
+++ src/main/java/com/thiagoretondar/XmlWithXpathApplication.java
@@ -0,0 +1,12 @@
+package com.thiagoretondar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlWithXpathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlWithXpathApplication.class, args);
+	}
+}
